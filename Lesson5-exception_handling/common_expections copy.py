841b6dcb9fa174a7c911545e187a5e456b348692
PresleyHe <prehe28@bergen.org>
2025-11-21 08:56:53 -0500

    expections.py

---
+++ Lesson5-exception_handling/common_expections copy.py
@@ -0,0 +1,36 @@
+# COmmon errors
+# 1 - Division by zero
+# print(10/0)
+
+# 2-ValueError
+# print(int("abc"))
+
+# 3-KeyError
+# grades = {"Bob":90}
+# print(grades["Bill"])
+
+# 4-IndexError
+# numbers = [1,2,3]
+# print(numbers[5])
+
+# # 5-NameError
+# print(total)
+# total = 10
+
+# SyntaxError
+# for i in range(5)
+# print(i)
+
+# AttributError
+# x = 5
+# x.append(3)
+
+# ModuleNotFoundError
+# import wonka_factory
+
+# IndentationError 
+# if i > 0:
+# return i
+
+# TypeError
+# print("hello" + 5)
--- Lesson5-exception_handling/expections.py
+++ Lesson5-exception_handling/expections.py
@@ -1,22 +1,64 @@
-# COmmon errors
-# 1 - Division by zero
-# print(10/0)
+def safe_divide(a, b):
+    try:
+        result = a / b
+        return result
+    # except: 
+    #     print("Can not divide by zero!")
+    except ZeroDivisionError: 
+        print("Can not divide by zero!")
+        return None
+    except TypeError:
+        print("That's not a valid number!")
+        return None
+    except:
+        print("An error occured...")
+print(safe_divide(10,2)) #5.0
+print(safe_divide(10,0))
+print(safe_divide(10, "hello"))
 
-# 2-ValueError
-# print(int("abc"))
+def safe_operations(a,b,lst,key,d):
+    try:
+        print(f"Division result: {a/b}")#ZerodivisionError
+        print("Access list element: ", lst[2])#IndexError
+        print("Access dictionary key: ", d[key])#KeyError
+        print(f"Add numbers: {a + b}")# TypeError
+    # except ZeroDivisionError:
+    #     print("Can not divide by zero!")
+    # except IndexError:
+    #     print("List index out of range")
+    # except KeyError:
+    #     print(f"Key: {key} not found in dictionary!")
+    # # except TypeError:
+    # #     print("Invalid types for operation!")
+    except Exception as e:
+        print("Some other error occured", e)
+    
+    
+    
+    
+    
+print(safe_operations(10,2,[1,2],"Tom", {"John": 15}))
+print(safe_operations(10,0,[1,2],"Tom", {"John": 15}))
+print(safe_operations(10,"hello" ,[1,2],"Tom", {"Tom": 15}))
 
-# 3-KeyError
-# grades = {"Bob":90}
-# print(grades["Bill"])
 
-# 4-IndexError
-# numbers = [1,2,3]
-# print(numbers[5])
+def calculate_price_per_item(cost, num):
+    try:
+        total = cost / num
+        
+    except ZeroDivisionError:
+        return "Can not divide by zero"
+    return total
 
-# # 5-NameError
-# print(total)
-# total = 10
+print(calculate_price_per_item(100,4))
+print(calculate_price_per_item(50,0))
+print(calculate_price_per_item(25.50,3))
 
-# SyntaxError
-# for i in range(5)
-# print(i)
+def parse_age(age):
+    try:
+        age = int(age)
+        return age
+    except ValueError:
+        print("Can only use string!")
+        return None
+print(parse_age("25"))
